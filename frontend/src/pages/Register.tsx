208cb9e32df62b4aa954ca7590a39140d532a1d7
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-12-04 22:36:18 -0300

    fix: Bug no endpoint

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -15,7 +15,7 @@ const Register: React.FC = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:3000/register", {
+      const response = await fetch("http://localhost:3000/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
