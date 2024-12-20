a8c5df4e2838657d5750044060471d458c2b39f8
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-12-19 23:13:49 -0300

    fix: Atualiza a renderização com base no estado de autenticação

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate(); // Para redirecionar o usuário após o logout
+  const location = useLocation(); // Para verificar a URL atual
 
-  // Verifica se o token está presente no localStorage quando a página carrega
+  // Verifica se o token está presente no localStorage sempre que a página carregar ou quando o token for alterado
   useEffect(() => {
     const token = localStorage.getItem("token");
     setIsAuthenticated(!!token); // Atualiza o estado com base no token
-  }, []); // Esse useEffect será executado apenas uma vez ao carregar o componente
+  }, [location.pathname]); // Atualiza sempre que o path mudar, o que ajuda a refletir alterações no estado
 
   // Função para fazer logout
   const handleLogout = () => {
@@ -17,6 +18,11 @@ const Navbar: React.FC = () => {
     setIsAuthenticated(false); // Atualiza o estado para não autenticado
     navigate("/login"); // Redireciona o usuário para a página de login
   };
+
+  // Condição para não mostrar a navbar nas páginas de login e register
+  if (location.pathname === '/login' || location.pathname === '/register') {
+    return null; // Não renderiza a Navbar nessas páginas
+  }
 
   return (
     <nav className="bg-[#0BB3D9] p-4">
